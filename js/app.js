4e415282d159f90dd8af660c459ec47c495f49a7
Karim Sami <karimsami047@gmail.com>
2020-07-22 18:08:37 +0200

    add all functionality and styles

--- js/app.js
+++ js/app.js
@@ -17,87 +17,14 @@
  * Define Global Variables
  *
  */
-const sections = [
-  ` <div class="landing__container">
-        <h2>Section 1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
-        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-      </div>
-    `,
-  ` <div class="landing__container">
-        <h2>Section 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-
-        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-      </div>
-    `,
-  ` <div class="landing__container">
-        <h2>Section 3</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-
-        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-      </div>
-    `,
-];
-const sectionsUl = document.querySelector("#sections_list");
+const sectionsUl = document.querySelectorAll("section");
 const navUl = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-c = 1;
-for (section of sections) {
-  let li = document.createElement("li");
-  let sec = document.createElement("section");
-  idAttribute = document.createAttribute("id");
-  idAttribute.value = `section${c}`;
-  dataAttribute = document.createAttribute("data-nav");
-  dataAttribute.value = `Section ${c}`;
-  sec.setAttributeNode(idAttribute);
-  sec.setAttributeNode(dataAttribute);
-  sec.innerHTML = section;
-  li.appendChild(sec);
-  sectionsUl.appendChild(li);
-  c += 1;
-}
-
-// build the nav
-
-c = 1;
-for (section of sections) {
-  let li = document.createElement("li");
-  let a = document.createElement("a");
-  hrefAttribute = document.createAttribute("href");
-  hrefAttribute.value = `#section${c}`;
-  a.setAttributeNode(hrefAttribute);
-  a.textContent = `Section ${c}`;
-  li.appendChild(a);
-  navUl.appendChild(li);
-  c += 1;
-}
-// Add class 'active' to section when near top of viewport
-
-// window.addEventListener("scroll", () => {
-//   const sections = document.querySelectorAll("section");
-//   sections.forEach((el) => {
-//     position = el.getBoundingClientRect();
-//     if (position.top >= 20 && position.bottom <= window.innerHeight) {
-//       el.classList.add("your-active-class");
-//     } else {
-//       el.classList.remove("your-active-class");
-//     }
-//   });
-// });
-
 let observer = new IntersectionObserver(
   (entries) => {
     if (entries[0].isIntersecting === true) {
@@ -109,12 +36,48 @@ let observer = new IntersectionObserver(
   { threshold: [0.5] }
 );
 
-document.querySelectorAll("section").forEach((el) => {
-  observer.observe(el);
-});
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+const buildNav = () => {
+  sectionsUl.forEach((el) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    hrefAttribute = document.createAttribute("href");
+    hrefAttribute.value = el.id;
+    a.setAttributeNode(hrefAttribute);
+    a.textContent = el.dataset.nav;
+    li.appendChild(a);
+    navUl.appendChild(li);
+  });
+};
+
+// Add class 'active' to section when near top of viewport
+
+const activate = () => {
+  sectionsUl.forEach((el) => {
+    observer.observe(el);
+  });
+};
 
 // Scroll to anchor ID using scrollTO event
-
+const scrollToSection = () => {
+  anchors = document.querySelectorAll("a");
+  anchors.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const section = document.querySelector(
+        `#${e.target.getAttribute("href")}`
+      );
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+};
 /**
  * End Main Functions
  * Begin Events
@@ -122,7 +85,8 @@ document.querySelectorAll("section").forEach((el) => {
  */
 
 // Build menu
-
+buildNav();
 // Scroll to section on link click
-
+scrollToSection();
 // Set sections as active
+activate();
